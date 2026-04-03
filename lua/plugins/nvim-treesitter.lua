ef37605d3e4aef673bfb82b798b9a3fe05788176
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-04 01:47:29 +0200

    Add nvim-treesitter plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  -- specify correct module to call .setup(opts) on
+  main = "nvim-treesitter.configs",
+  opts = require "config.nvim-treesitter",
+  lazy = false,
+  -- TODO: remove this after updating to 0.12
+  branch = "master",
+}
