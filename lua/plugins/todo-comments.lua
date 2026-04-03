ad532a54e4648971d81c27bf453d455d8bd06c7b
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-04 00:46:29 +0200

    Add todo-comments plugin

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,14 @@
+return {
+  -- Inserts colourful TODO, etc notes.
+  -- TODO: test
+  -- PERF : test
+  -- NOTE: test
+  -- FIX: test
+  -- WARN: test
+  -- HACK: test
+  "folke/todo-comments.nvim",
+  event = "BufEnter",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  -- accept defaults
+  opts = {},
+}
