8f20ee92b320defec223741554e816f60f1b2f0b
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-03 04:13:48 +0200

    Add folke's tokyonight theme plugin

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false, -- load on startup
+  priority = 1000, -- load this before all other start plugins
+  config = function()
+    -- load the colorscheme here
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
