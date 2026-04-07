eae13c5b5c53af32f4b69bba2087e0919f4ccb39
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-07 04:16:56 +0200

    Add diagnostic highlighting

--- init.lua
+++ init.lua
@@ -22,3 +22,14 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 2,        -- columns between code and the message
+    -- prefix = "●",        -- no leading symbol; set to "●" or "▎" if you want one
+    -- source = "if_many",  -- show "[lua_ls]" etc. when multiple servers attach
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
