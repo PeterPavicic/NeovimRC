69c5fd0fe35ec1c4f2f41a8c979ecb68bff515f0
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-07 02:12:17 +0200

    Set up LSPs using nvim-lspconfig

---
+++ lua/config/nvim-lspconfig.lua
@@ -0,0 +1,45 @@
+-- In order for these to work make sure to install the corresponding LSPs (potentially using Mason)
+local servers = {
+  "bashls",
+  "cssls",
+  "hyprls",
+  "jdtls",
+  "jsonls",
+  "lua_ls",
+  "marksman",
+  "prettier",
+  "pyright",
+  "r_language_server",
+  "texlab",
+  "tinymist",
+}
+
+
+local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+
+-- Enable basic capabilities for all LSPs
+vim.lsp.config('*', {
+  capabilities = default_capabilities,
+})
+
+-- lower highlight priority for highlights than treesitter
+-- meaning this will fire if treesitter errors/fails/should work for language injection
+vim.highlight.priorities.semantic_tokens = 95
+
+-- Make sure lua detects Neovim API
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+      },
+    },
+  }
+})
+
+vim.lsp.enable(servers)
