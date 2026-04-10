628147710344e9288dd5f86e9d070d39f2a8222d
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-10 12:48:17 +0200

    Migrate to main branch of nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,9 +3,41 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   -- specify correct module to call .setup(opts) on
-  main = "nvim-treesitter.configs",
-  opts = require "config.nvim-treesitter",
+  main = "nvim-treesitter",
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    local ensure_installed = {
+        "bash",
+        "css",
+        "csv",
+        "graphql",
+        "html",
+        "java",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "r",
+        "rnoweb",
+        "yaml",
+        "sql",
+        "typst",
+    }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensure_installed)
+    :filter(function(parser)
+      return not vim.tbl_contains(alreadyInstalled, parser)
+    end)
+    :totable()
+  require('nvim-treesitter').install(parsersToInstall)
+  end,
   lazy = false,
-  -- TODO: remove this after updating to 0.12
-  branch = "master",
 }
