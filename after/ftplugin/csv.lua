affd0262a327a72e0be1772f2bc6fd2390f933ba
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-23 22:50:03 +0200

    Add csvview & its keymaps

---
+++ after/ftplugin/csv.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<LocalLeader>t", "<Cmd>CsvViewToggle<CR>",  { buffer = true, desc = "CSV toggle view" })
+vim.keymap.set("n", "<LocalLeader>e", "<Cmd>CsvViewEnable<CR>",  { buffer = true, desc = "CSV enable view" })
+vim.keymap.set("n", "<LocalLeader>d", "<Cmd>CsvViewDisable<CR>", { buffer = true, desc = "CSV disable view" })
+vim.keymap.set("n", "<LocalLeader>i", "<Cmd>CsvViewInfo<CR>",    { buffer = true, desc = "CSV view info" })
