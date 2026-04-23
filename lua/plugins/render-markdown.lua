181d4ecdcad64b61ff7435ff3984e3e616589ad6
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-24 01:46:53 +0200

    Add render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = {"markdown", "quarto"},
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
