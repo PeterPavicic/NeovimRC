f6657424a890a7839204d291d56f756c4914a6af
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-23 23:11:06 +0200

    Add bufferline (enable on <leader>tb)

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  'akinsho/bufferline.nvim',
+  lazy = true,
+  version = "*",
+  dependencies = {"nvim-tree/nvim-web-devicons"},
+  opts = {},
+  keys = "<leader>tb"
+}
