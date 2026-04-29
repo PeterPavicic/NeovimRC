32c1f35e7532af3e2f2d01ecbabc24d73dcedf16
Peter Pavicic <peter.pavicic@gmail.com>
2026-04-29 19:38:09 +0200

    FIX: autoload csvview on entering csv file

--- after/ftplugin/csv.lua
+++ after/ftplugin/csv.lua
@@ -1,4 +1,2 @@
+require("csvview").enable()
 vim.keymap.set("n", "<LocalLeader>t", "<Cmd>CsvViewToggle<CR>", { buffer = true, desc = "CSV toggle view" })
-vim.keymap.set("n", "<LocalLeader>e", "<Cmd>CsvViewEnable<CR>", { buffer = true, desc = "CSV enable view" })
-vim.keymap.set("n", "<LocalLeader>d", "<Cmd>CsvViewDisable<CR>", { buffer = true, desc = "CSV disable view" })
-vim.keymap.set("n", "<LocalLeader>i", "<Cmd>CsvViewInfo<CR>", { buffer = true, desc = "CSV view info" })
