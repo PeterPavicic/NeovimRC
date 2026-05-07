5e666a82ec089b53ae1baa54a41f2532774ec5a9
Peter Pavicic <peter.pavicic@gmail.com>
2026-05-08 00:17:39 +0200

    CHORE: Code cleanup on lua/mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,118 +1,122 @@
+local map = vim.keymap.set
+
 -- Avoid accidentally closing everything
-vim.keymap.set("i", "<C-c>", "<ESC>")
+map("i", "<C-c>", "<ESC>")
 
 -- move text around using J, K
--- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block up 1 line"})
--- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block down 1 line"})
+-- map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block up 1 line"})
+-- map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block down 1 line"})
 
 -- Navigate between windows in any modes
-vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
-vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
-vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
-vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
-vim.keymap.set({ "n" }, "<A-h>", "<C-w>h")
-vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
-vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
-vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
-vim.keymap.set({ "n" }, "<A-Left>", "<C-w>h")
-vim.keymap.set({ "n" }, "<A-Down>", "<C-w>j")
-vim.keymap.set({ "n" }, "<A-Up>", "<C-w>k")
-vim.keymap.set({ "n" }, "<A-Right>", "<C-w>l")
+map({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
+map({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
+map({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
+map({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
+map({ "n" }, "<A-h>", "<C-w>h")
+map({ "n" }, "<A-j>", "<C-w>j")
+map({ "n" }, "<A-k>", "<C-w>k")
+map({ "n" }, "<A-l>", "<C-w>l")
+map({ "n" }, "<A-Left>", "<C-w>h")
+map({ "n" }, "<A-Down>", "<C-w>j")
+map({ "n" }, "<A-Up>", "<C-w>k")
+map({ "n" }, "<A-Right>", "<C-w>l")
 
 -- J does not move cursor
-vim.keymap.set("n", "J", function()
+map("n", "J", function()
   local pos = vim.api.nvim_win_get_cursor(0)
   vim.cmd("normal! J")
   vim.api.nvim_win_set_cursor(0, pos)
 end, { desc = "Collapse next line" })
 
 -- Center when moving up, down half screen
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half-page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half-page up" })
 
 -- Center when searching
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result" })
+map("n", "n", "nzzzv", { desc = "Next search result" })
+map("n", "N", "Nzzzv", { desc = "Previous search result" })
 
 -- greatest remap ever (paste no replace)
-vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste and keep yank" })
+map("x", "<leader>p", '"_dP', { desc = "Paste and keep yank" })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete into void" })
-vim.keymap.set({ "n", "v" }, "<leader>x", '"_x', { desc = "Delete into void" })
+map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete into void" })
+map({ "n", "v" }, "<leader>x", '"_x', { desc = "Delete into void" })
 
 -- next greatest remap ever (yank into system clipboard)
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard" })
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard" })
 -- paste to/from system clipboard
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
-vim.keymap.set("n", "<leader>P", '"+P', { desc = "Paste from system clipboard" })
+map("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+map("n", "<leader>P", '"+P', { desc = "Paste from system clipboard" })
 
 -- avoid Ex mode
-vim.keymap.set("n", "Q", "<nop>")
+map("n", "Q", "<nop>")
 
 -- Open :h help for word under cursor
--- vim.keymap.set("n", "<leader>h", [[:h <<C-r><C-w><CR>]], { desc = "Search word under cursor in manual"} )
+-- map("n", "<leader>h", [[:h <<C-r><C-w><CR>]], { desc = "Search word under cursor in manual"} )
 
 -- open/close tabs
-vim.keymap.set("n", "<leader>T", "<CMD>tabnew<CR>", { desc = "Open new tab" })
-vim.keymap.set("n", "<leader>Q", "<CMD>tabclose<CR>", { desc = "Close current tab" })
+map("n", "<leader>T", "<CMD>tabnew<CR>", { desc = "Open new tab" })
+map("n", "<leader>Q", "<CMD>tabclose<CR>", { desc = "Close current tab" })
 
 -- open/close buffers
-vim.keymap.set("n", "<leader>B", "<CMD>enew<CR>", { desc = "Open new buffer" })
-vim.keymap.set("n", "<leader>q", "<CMD>bdelete<CR>", { desc = "Close current buffer" })
+map("n", "<leader>B", "<CMD>enew<CR>", { desc = "Open new buffer" })
+map("n", "<leader>q", "<CMD>bdelete<CR>", { desc = "Close current buffer" })
 
 -- save/copy file
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<C-c>", "<cmd>%y<CR>", { desc = "Copy whole file" })
-vim.keymap.set("n", "<leader><C-c>", "<cmd>%y+<CR>", { desc = "Copy whole file to system clipboard" })
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<C-c>", "<cmd>%y<CR>", { desc = "Copy whole file" })
+map("n", "<leader><C-c>", "<cmd>%y+<CR>", { desc = "Copy whole file to system clipboard" })
 
 -- Comment
-vim.keymap.set("n", "<leader>/", "gcc", { desc = "Toggle comment", remap = true })
-vim.keymap.set("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
+map("n", "<leader>/", "gcc", { desc = "Toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
 
 -- Clear highlights after search
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 -- LSP mappings
-vim.keymap.set("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 
--- vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
--- vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
-
--- telescope
-vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
-vim.keymap.set(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "telescope find all files" }
-)
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "telescope find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
-vim.keymap.set("n", "<leader>ft", "<cmd>Telescope<CR>", { desc = "Open telescope (colourscheme)" })
-vim.keymap.set(
+-- TELESCOPE
+map("n", "<leader>ft", require("telescope.builtin").builtin, { desc = "Open telescope (colourscheme)" })
+--
+-- buffers
+map("n", "<leader>b", require("telescope.builtin").buffers, { desc = "telescope find buffers" })
+--
+-- find files
+map("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "telescope find files" })
+map("n", "<leader>fa", function()
+  require("telescope.builtin").find_files({ follow = true, no_ignore = true, hidden = true })
+end, { desc = "telescope find all files" })
+--
+-- grep
+map("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "telescope live grep" })
+map(
   "n",
   "<leader>fz",
-  "<cmd>Telescope current_buffer_fuzzy_find<CR>",
+  require("telescope.builtin").current_buffer_fuzzy_find,
   { desc = "telescope find in current buffer" }
 )
-vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
-vim.keymap.set("n", "<leader>gst", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
-vim.keymap.set("n", "<leader>th", "<cmd>Telescope colorscheme<CR>", { desc = "telescope theme (colourscheme)" })
+--
+-- help
+map("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "telescope help page" })
+map("n", "<leader>gc", require("telescope.builtin").git_commits, { desc = "telescope git commits" })
+map("n", "<leader>gst", require("telescope.builtin").git_status, { desc = "telescope git status" })
+map("n", "<leader>th", require("telescope.builtin").colorscheme, { desc = "telescope theme (colourscheme)" })
 
 -- undotree
-vim.keymap.set("n", "<leader>u", require("undotree").open, { desc = "Toggle undotree" })
+map("n", "<leader>u", require("undotree").open, { desc = "Toggle undotree" })
 
 -- oil
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader>-", "<CMD>Oil --float<CR>", { desc = "Open oil in floating window" })
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map("n", "<leader>-", "<CMD>Oil --float<CR>", { desc = "Open oil in floating window" })
 
 -- conform
-vim.keymap.set("n", "<leader>fmt", require("conform").format, { desc = "Format buffer (conform)" })
-vim.keymap.set("n", "<leader>tf", function()
+map("n", "<leader>fmt", require("conform").format, { desc = "Format buffer (conform)" })
+map("n", "<leader>tf", function()
   if vim.g.disable_autoformat then
     vim.g.disable_autoformat = false
     vim.notify("Format on save enabled")
