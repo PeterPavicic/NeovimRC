c7a46dd1dcdc9afefd1d45d710fe5e256a352e66
Peter Pavicic <peter.pavicic@gmail.com>
2026-06-16 08:46:41 +0200

    Fix error messages on Termux

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,43 +2,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  -- specify correct module to call .setup(opts) on
   main = "nvim-treesitter",
-  init = function()
+  opts = {
+    ensure_installed = {
+      "bash", "css", "csv", "graphql", "html",
+      "java", "json", "lua", "markdown", "python",
+      "r", "rnoweb", "yaml", "sql", "typst",
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter").setup(opts)
+
     vim.api.nvim_create_autocmd("FileType", {
       callback = function()
-        -- Enable treesitter highlighting and disable regex syntax
         pcall(vim.treesitter.start)
-        -- Enable treesitter-based indentation
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
-
-    local ensure_installed = {
-      "bash",
-      "css",
-      "csv",
-      "graphql",
-      "html",
-      "java",
-      "json",
-      "lua",
-      "markdown",
-      "python",
-      "r",
-      "rnoweb",
-      "yaml",
-      "sql",
-      "typst",
-    }
-    local alreadyInstalled = require("nvim-treesitter.config").get_installed()
-    local parsersToInstall = vim
-      .iter(ensure_installed)
-      :filter(function(parser)
-        return not vim.tbl_contains(alreadyInstalled, parser)
-      end)
-      :totable()
-    require("nvim-treesitter").install(parsersToInstall)
   end,
   lazy = false,
 }
