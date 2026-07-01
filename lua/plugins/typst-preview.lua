61631ec5139374a9dcbc239c442e6e6154e1e4c7
Peter Pavicic <peter.pavicic@gmail.com>
2026-07-01 23:20:16 +0200

    Open Typst preview in new firefox window

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -2,6 +2,10 @@
 return {
   "chomosuke/typst-preview.nvim",
   version = "1.*",
-  opts = {}, -- default options
+  opts = {
+    -- Custom format string to open the output link provided with %s
+    -- Example: open_cmd = 'firefox %s -P typst-preview --class typst-preview'
+    open_cmd = "firefox --new-window %s --class typst-preview",
+  }, -- default options
   ft = "typst", -- lazy-load only for Typst files
 }
